65065435897cb6ad6f58c97a09c169e1c4113aaa
Patrick Kelly <patrick@pkelly.co>
2018-02-02 17:02:32 +0000

    added auth to auth.js

---
+++ controllers/auth.js
@@ -0,0 +1,26 @@
+const User = require('../models/user');
+const jwt = require('jsonwebtoken');
+const { secret } = require('../config/environment');
+
+function register(req, res) {
+  User
+    .create(req.body)
+    .then(() => res.json({ message: 'Registration successful'}));
+}
+
+function login(req, res) {
+  User
+    .findOne({ email: req.body.email })
+    .then((user) => {
+      if(!user || !user.validatePassword(req.body.password)) return res.status(401).json({ message: 'Unauthorized' });
+
+      const token = jwt.sign({ userId: user.id }, secret, { expiresIn: '1hr' });
+      console.log(token);
+      return res.json({ token, message: `Welcome back ${user.name}` });
+    });
+}
+
+module.exports = {
+  register,
+  login
+};
