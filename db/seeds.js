cce7427a5cc15469699545bf752a0f535a5e9074
EvelinaKuu <evelina.kuusinen@gmail.com>
2018-02-03 12:33:51 +0000

    error on line 72 and dates not set

---
+++ db/seeds.js
@@ -0,0 +1,106 @@
+const mongoose  = require('mongoose');
+const bluebird  = require('bluebird');
+const User = require('../models/user');
+const Holiday = require('../models/holiday');
+
+mongoose.Promise = require('bluebird');
+const { db }    = require('../config/environment');
+
+
+mongoose.Promise = bluebird;
+mongoose.connect(db);
+
+User.collection.drop();
+Holiday.collection.drop();
+
+User
+  .create([{
+    name: 'Shaun White',
+    email: 'shaun@ga.co',
+    password: 'shaun',
+    sport: 2,
+    abilityLevel: 3,
+    image: 'https://cdn-s3.si.com/s3fs-public/sikids/pages/images/cms/imce/users/dantec/2014/01/shaun-white-2014-olympics-header.jpg'
+  }, {
+    name: 'Vreni Schneider',
+    email: 'vreni@ga.co',
+    password: 'vreni',
+    sport: 1,
+    abilityLevel: 3,
+    image: 'http://cdn.welove2ski.com/wp-content/uploads/vreni1-1.jpg'
+  }, {
+    name: 'Tracis Rice',
+    email: 'tracis@ga.co',
+    password: 'tracis',
+    sport: 2,
+    abilityLevel: 3,
+    image: 'https://assets.saatchiart.com/saatchi/84381/art/1617509/819762-7.jpg'
+  },{
+    name: 'Hannah Teter',
+    email: 'hannah@ga.co',
+    password: 'hannah',
+    sport: 2,
+    abilityLevel: 3,
+    image: 'https://www.sierrasun.com/wp-content/uploads/2017/12/hannahteter-tdt-120917-1-4.jpg'
+  },{
+    name: 'Charlotte',
+    email: 'charlotte@ga.co',
+    password: 'charlotte',
+    sport: 1,
+    abilityLevel: 2,
+    image: 'http://www.oogazone.com/wp-content/uploads/top-little-girl-skiing-stock-vector-skier-cartoon-ski-image.jpg'
+  },{
+    name: 'Evelina',
+    email: 'evelina@ga.co',
+    password: 'evelina',
+    sport: 1,
+    abilityLevel: 2,
+    image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSXf30fSEExqSEGeCRv9gMYTlR1CLP3pybH0chBM77ACjUqsbRm'
+  },{
+    name: 'Patrick',
+    email: 'patrick@ga.co',
+    password: 'patrick',
+    sport: 1,
+    abilityLevel: 1,
+    image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvvdYy2pa7Es35cKnm_MhPGLDH1rzlPu0C3Dybe8AeNVYKsdw'
+  }])
+  .then((users) => {
+    console.log(`${users.length} users created!`);
+
+        return Holiday.create([{
+          name: 'Patrick turns 25 ski trip',
+          attendees: 'Charlotte', 'Evelina', 'Hannah Teter', 'Shaun White', 'Vreni Schneider', 'Tracis Rice',
+          dates: '',
+          destination: 'Chamonix',
+          createdBy: users[6],
+          comments: [{
+           comment: 'This is going to be epic!',
+           createdBy: users[1]
+         },{
+           comment: 'Cannot wait to start planning!',
+           createdBy: users[0]
+         }]
+        },{
+          name: 'Shaun & girls go ',
+          attendees: 'Charlotte', 'Evelina', 'Hannah Teter', 'Shaun White', 'Vreni Schneider', 'Tracis Rice',
+          dates: '',
+          destination: 'Chamonix',
+          createdBy: users[0],
+          comments: [{
+           comment: ' Where do we want to eat the first night?!',
+           createdBy: users[5]
+         },{
+           comment: 'I shoud get new skies, to rent or buy? 🧐',
+           createdBy: users[4]
+         }]
+        }
+  .catch(err => console.log(err))
+  .finally(() => mongoose.connection.close());
+
+  .then((holidays) => {
+  console.log(`${holidays.length} holidays created!`);
+})
+.finally(() => {
+  return mongoose.connection.close();
+})
+.catch(err => console.log(err));
