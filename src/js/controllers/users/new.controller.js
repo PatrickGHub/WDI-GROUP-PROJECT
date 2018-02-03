26ff8ad848e1b5cedee02fe33dda220b596ee070
Patrick Kelly <patrick@pkelly.co>
2018-02-03 12:32:51 +0000

    created user routes

---
+++ src/js/controllers/users/new.controller.js
@@ -0,0 +1,18 @@
+angular
+  .module('appres')
+  .controller('UsersNewCtrl', UsersNewCtrl);
+
+UsersNewCtrl.$inject = ['UserFactory', '$state'];
+function UsersNewCtrl(UserFactory, $state) {
+  const vm = this;
+
+  vm.newUser    = {};
+  vm.create     = create;
+
+  function create(){
+    UserFactory
+      .save(vm.newUser)
+      .$promise
+      .then(() => $state.go('login'));
+  }
+}
