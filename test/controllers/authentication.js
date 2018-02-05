2db02a669336e0d62720db1914ec32da2a563dc2
“CharlotteMD” <charlottemdavies@hotmail.co.uk>
2018-02-05 11:18:59 +0000

    3/4 tests working for user authentication

--- test/controllers/authentication.js
+++ test/controllers/authentication.js
@@ -1,4 +1,4 @@
-/* globals api, expect, describe, it*/
+/* globals api, expect, describe, it, afterEach, beforeEach */
 require('../spec_helper');
 
 const User = require('../../models/user');
@@ -6,7 +6,7 @@ const User = require('../../models/user');
 describe('Authentication Controller Tests', () => {
 
   afterEach(done => {
-    User.collection.remove();
+    User.collection.drop();
     done();
   });
 
@@ -17,10 +17,10 @@ describe('Authentication Controller Tests', () => {
         .post('/api/register')
         .set('Accept', 'application/json')
         .send({
-          username: 'test',
-          email: 'test@test.com',
+          username: 'Development',
+          email: 'guy@guy.com',
           password: 'password',
-          // passwordConfirmation: 'password',
+          passwordConfirmation: 'password',
           sport: 1,
           abilityLevel: 1,
           image: 'http://#'
@@ -33,75 +33,86 @@ describe('Authentication Controller Tests', () => {
           done();
         });
     });
-  });
 
-  //   it('should not register a user if password and passwordConfirmation do not match', done => {
-  //     api
-  //       .post('/api/register')
-  //       .set('Accept', 'application/json')
-  //       .send({
-  //         username: 'test',
-  //         email: 'test@test.com',
-  //         password: 'password'
-  //         // passwordConfirmation: 'passworddd'
-  //       })
-  //       .end((err, res) => {
-  //         expect(res.status).to.eq(400);
-  //         expect(res.body).to.be.a('object');
-  //         expect(res.body.message).to.eq('Bad Request');
-  //         // expect(res.body.errors).to.eq('ValidationError: passwordConfirmation: does not match');
-  //         done();
-  //       });
-  //   });
-  // });
-
-  // LOGIN ROUTE
-  describe('POST /api/login', () => {
-    beforeEach(done => {
+    it('should not register a user if password and passwordConfirmation do not match', done => {
       api
         .post('/api/register')
         .set('Accept', 'application/json')
         .send({
-          email: 'test@test.com',
-          password: 'password'
-        })
-        .end(() => {
-          done();
-        });
-    });
-
-    it('should login a user with the correct credentials', done => {
-      api
-        .post('/api/login')
-        .set('Accept', 'application/json')
-        .send({
-          email: 'test@test.com',
-          password: 'password'
+          username: 'Development',
+          email: 'hello@test.com',
+          password: 'password',
+          passwordConfirmation: 'forgot',
+          sport: 1,
+          abilityLevel: 1,
+          image: 'http://#'
         })
         .end((err, res) => {
-          expect(res.status).to.eq(200);
+          expect(res.status).to.eq(400);
           expect(res.body).to.be.a('object');
-          expect(res.body.message).to.eq(`Welcome back ${res.body.user.username}`);
-          expect(res.body.token).to.be.a('string');
-          done();
-        });
-    });
-
-    it('should not login a user with incorrect credentials', function(done) {
-      api
-        .post('/api/login')
-        .set('Accept', 'application/json')
-        .send({
-          email: 'test@test.com',
-          password: 'passworddd'
-        })
-        .end((err, res) => {
-          expect(res.status).to.eq(401);
-          expect(res.body).to.be.a('object');
-          expect(res.body.message).to.eq('Unauthorized');
-          expect(Object.keys(res.body)).to.not.include('token');
+          expect(res.body.message).to.eq('Bad Request');
+          expect(res.body.errors).to.eq('ValidationError: passwordConfirmation: does not match');
           done();
         });
     });
   });
+
+});
+
+// LOGIN ROUTE
+describe('POST /api/login', () => {
+  beforeEach(done => {
+    api
+      .post('/api/register')
+      .set('Accept', 'application/json')
+      .send({
+        username: 'Development',
+        email: 'hello@test.com',
+        password: 'password',
+        passwordConfirmation: 'password',
+        sport: 1,
+        abilityLevel: 1,
+        image: 'http://#'
+      })
+      .end(() => {
+        done();
+      });
+  });
+
+  it('should login a user with the correct credentials', done => {
+    api
+      .post('/api/login')
+      .set('Accept', 'application/json')
+      .send({
+        email: 'hello@test.com',
+        password: 'password'
+      })
+      .end((err, res) => {
+        expect(res.status).to.eq(200);
+        expect(res.body).to.be.a('object');
+        expect(res.body.message).to.eq('Welcome back Development');
+        expect(res.body.token).to.be.a('string');
+        done();
+      });
+  });
+
+  // it('should login a user with the correct credentials', done => {
+  //   api
+  //     .post('/api/login')
+  //     .set('Accept', 'application/json')
+  //     .send({
+  //       email: 'test@test.com',
+  //       password: 'password'
+  //     })
+  // .end((err, res) => {
+  //   expect(res.status).to.eq(200);
+  //   expect(res.body).to.be.a('object');
+  //   expect(res.body.message).to.eq(`Welcome back ${res.body.user.username}`);
+  //   expect(res.body.token).to.be.a('string');
+  //   done();
+  // });
+// });
+
+
+
 });
