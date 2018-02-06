470793679ab48d3cd7cae31bf13799cc1bfead7c
Patrick Kelly <patrick@pkelly.co>
2018-02-06 12:04:11 +0000

    added holiday delete, began to create holiday index

--- src/js/controllers/holidays/edit.controller.js
+++ src/js/controllers/holidays/edit.controller.js
@@ -9,12 +9,21 @@ function HolidaysEditCtrl(HolidayFactory, $state) {
   vm.holiday = HolidayFactory.get($state.params);
   vm.update  = update;
 
-
   function update(){
     HolidayFactory
       .update($state.params.id, vm.holiday)
       .$promise
       .then(() => $state.go('holidaysShow'));
+  }
+
+  vm.delete = holidaysDelete;
+
+  function holidaysDelete(){
+    HolidayFactory.delete($state.params)
+      .$promise
+      .then(() => {
+        $state.go('holidaysIndex');
+      });
   }
 }
 
