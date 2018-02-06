30b470edea694f7c634038c91ebc1c1202b86ee7
EvelinaKuu <evelina.kuusinen@gmail.com>
2018-02-06 10:22:54 +0000

    added EDIT PROFILE to navbar

---
+++ src/js/controllers/main.controller.js
@@ -0,0 +1,17 @@
+angular
+  .module('appres')
+  .controller('MainCtrl', MainCtrl);
+
+MainCtrl.$inject = ['$transitions', '$rootScope', '$state', '$auth'];
+function MainCtrl($transitions, $rootScope, $state, $auth) {
+  const vm = this;
+
+  vm.isAuthenticated = $auth.isAuthenticated;
+  vm.logout = logout;
+  vm.currentUserId = $auth.getPayload().userId;
+
+  function logout() {
+    $auth.logout();
+    $state.go('landing');
+  }
+}
--- src/js/controllers/users/edit.controller.js
+++ src/js/controllers/users/edit.controller.js
@@ -8,11 +8,17 @@ function UsersEditCtrl(UserFactory, $state) {
 
   vm.user   = UserFactory.get($state.params);
   vm.update = update;
+  vm.delete = destroy;
 
   function update(){
     UserFactory
       .update($state.params, vm.user)
       .$promise
       .then(() => $state.go('userEdit'));
+  }
+
+  function destroy() {
+    console.log("clicked");
+    // send request to api containing id for user that i want to delete
   }
 }
