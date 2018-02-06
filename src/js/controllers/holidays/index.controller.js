cf3c32bb031e390e1ebbe539002ed8eec0c49565
“CharlotteMD” <charlottemdavies@hotmail.co.uk>
2018-02-06 16:27:03 +0000

    added autocomplete to holiday index

--- src/js/controllers/holidays/index.controller.js
+++ src/js/controllers/holidays/index.controller.js
@@ -2,8 +2,16 @@ angular
   .module('appres')
   .controller('HolidaysIndexCtrl', HolidaysIndexCtrl);
 
-HolidaysIndexCtrl.$inject = ['HolidayFactory'];
-function HolidaysIndexCtrl(HolidayFactory) {
+HolidaysIndexCtrl.$inject = ['HolidayFactory', '$state'];
+function HolidaysIndexCtrl(HolidayFactory, $state) {
   const vm = this;
   vm.all = HolidayFactory.query();
+
+  vm.noResults = false;
+
+  vm.pickHoliday = pickHoliday;
+
+  function pickHoliday() {
+    $state.go('holidayShow', { id: vm.selectedHoliday.id });
+  }
 }
