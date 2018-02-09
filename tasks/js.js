54023ecc27d70bbdf9fc8736e826f887f1ef9e0a
Patrick Kelly <patrick@pkelly.co>
2018-02-09 10:46:14 +0000

    code no longer minifies

--- tasks/js.js
+++ tasks/js.js
@@ -41,7 +41,7 @@ const buildJs = () => {
   .pipe(gulpIf(global.production, replace('http://localhost:4000', process.env.API_URL)))
   .pipe(concat(config.output.js))
   .pipe(sourcemaps.init())
-  .pipe(gulpIf(global.production, uglify()))
+  // .pipe(gulpIf(global.production, uglify()))
   .pipe(gulpIf(global.production, rename({ suffix: '.min' })))
   .pipe(sourcemaps.write())
   .pipe(gulp.dest(config.dest.js))
